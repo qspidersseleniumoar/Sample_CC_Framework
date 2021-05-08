030322dad0dff996d05f754c443b093f179de7fa
qspidersseleniumoar <qspidersseleniumoar1234@gmail.com>
2021-05-08 17:41:49 +0530

    push

---
+++ SampeMaven_Framework/src/test/java/com/crm/comcast/ProductTest/CreateProductTest.java
@@ -0,0 +1,20 @@
+package com.crm.comcast.ProductTest;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest {
+	
+	
+	@Test(groups = "smokeTest")
+	public void createProductWithNAmeTest() {
+		System.out.println("execute createProductWithNAmeTest");
+ 	}
+
+	
+	
+	@Test(groups = "regressionTest")
+
+	public void createProductWithIDTest() {
+		System.out.println("execute createProductWithIDTest");
+ 	}
+}
---
+++ SampeMaven_Framework/src/test/java/com/crm/comcast/ProductTest/SearchProductTest.java
@@ -0,0 +1,19 @@
+package com.crm.comcast.ProductTest;
+
+import org.testng.annotations.Test;
+
+public class SearchProductTest {
+	
+	
+	@Test(groups = "smokeTest")
+	public void searchProductWithNAmeTest() {
+		System.out.println("execute searchProductWithNAmeTest");
+ 	}
+
+	
+	
+	@Test(groups = "regressionTest")
+	public void searchProductWithIDTest() {
+		System.out.println("execute searchProductWithIDTest");
+ 	}
+}
